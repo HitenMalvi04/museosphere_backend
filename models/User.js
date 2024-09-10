a68df64cb3f5f8eb92f65362587a70e79aaeda90
HitenMalvi04 <malvihiten8842@gmail.com>
2024-09-10 21:57:18 +0530

    small change

--- models/User.js
+++ models/User.js
@@ -12,12 +12,17 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('validate', async function (next) {
   
   console.log("Came here");
-  if (this.isNew) { // Only auto-increment for new documents
-    const latestUser = await mongoose.model('User').findOne().sort({ user_id: -1 });
-    this.user_id = latestUser ? latestUser.user_id + 1 : 2201; // Start user_id from 1
-    console.log("this.userid:::::::",this.user_id);
+  try {
+    if (this.isNew) { // Only auto-increment for new documents
+      const latestUser = await mongoose.model('User').findOne().sort({ user_id: -1 });
+      this.user_id = latestUser ? latestUser.user_id + 1 : 2205; // Start user_id from 2201
+      console.log("this.user_id:::::::", this.user_id);
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error); // Pass error to the next middleware
   }
-  next();
 });
 
 // Encrypt password before saving
